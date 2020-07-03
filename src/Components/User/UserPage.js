e87560e14b00b7d8ad65d46b37ba3f2cf3c6449b
James Sayer <james.sayer0908@gmail.com>
2020-07-03 13:20:01 -0700

    Fixed warnings

--- src/Components/User/UserPage.js
+++ src/Components/User/UserPage.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import {
-  Typography
-} from '@material-ui/core';
 
 import { DefaultLayout } from '../Layout';
 import UserList from './UserList';
 
 export default function UserPage() {
-  const [title, setTitle] = React.useState('Users');
+  const [title] = React.useState('Users');
 
   return (
     <DefaultLayout title={title}>
