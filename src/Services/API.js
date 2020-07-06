d104263bf423b4212567997c22604f362b290692
James Sayer <james.sayer0908@gmail.com>
2020-07-06 11:43:15 -0700

    More code formatting

--- src/Services/API.js
+++ src/Services/API.js
@@ -3,53 +3,53 @@ import Axios from './Axios';
 
 
 class CRUD {
-    static handleResponse(res) {
-      return res.data;
-    }
+  static handleResponse(res) {
+    return res.data;
+  }
 
-    static handleError(err) {
-      if (err.response) {
-        if (err.response.status === 401) {
-          Auth.logout();
-        } else {
-          console.error(err.message + ': ' + err.request.responseURL);
-        }
-      } else if (err.request) {
-          console.log(err.request);
+  static handleError(err) {
+    if (err.response) {
+      if (err.response.status === 401) {
+        Auth.logout();
       } else {
-          console.log(err);
+        console.error(err.message + ': ' + err.request.responseURL);
       }
-
-      return err;
+    } else if (err.request) {
+        console.log(err.request);
+    } else {
+        console.log(err);
     }
 
-    static create(user) {
-      return Axios
-        .post(`/${this.endpoint}`, user)
-        .then(this.handleResponse)
-        .catch(this.handleError);
-    }
+    return err;
+  }
 
-    static get(id) {
-      return Axios
-        .get(`/${this.endpoint}/${id}`)
-        .then(this.handleResponse)
-        .catch(this.handleError);
-    }
+  static create(user) {
+    return Axios
+      .post(`/${this.endpoint}`, user)
+      .then(this.handleResponse)
+      .catch(this.handleError);
+  }
 
-    static getAll() {
-      return Axios
-        .get(`/${this.endpoint}`)
-        .then(this.handleResponse)
-        .catch(this.handleError);
-    }
+  static get(id) {
+    return Axios
+      .get(`/${this.endpoint}/${id}`)
+      .then(this.handleResponse)
+      .catch(this.handleError);
+  }
 
-    static update(id, user) {
-      return Axios
-        .post(`/${this.endpoint}/${id}`, user)
-        .then(this.handleResponse)
-        .catch(this.handleError);
-    }
+  static getAll() {
+    return Axios
+      .get(`/${this.endpoint}`)
+      .then(this.handleResponse)
+      .catch(this.handleError);
+  }
+
+  static update(id, user) {
+    return Axios
+      .post(`/${this.endpoint}/${id}`, user)
+      .then(this.handleResponse)
+      .catch(this.handleError);
+  }
 }
 
 const checkAuth = () => {
