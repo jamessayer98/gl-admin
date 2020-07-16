f971728a61d39a8b0e5097841d06e1585e111942
James Sayer <james.sayer0908@gmail.com>
2020-07-15 17:16:06 -0700

    Added Currency component and more work on order details

--- src/Components/Modules/Order/OrderViewer.js
+++ src/Components/Modules/Order/OrderViewer.js
@@ -5,14 +5,21 @@ import {
   Card,
   IconButton,
   CardContent,
-  makeStyles
+  makeStyles,
+  TableContainer,
+  Box,
+  Table,
+  TableRow,
+  TableCell
 } from '@material-ui/core';
 import {
-  Edit as EditIcon
+  Edit as EditIcon,
+  Save as SaveIcon
 } from '@material-ui/icons';
 import API from '../../../Services/API';
 import { parseGLID } from '../../UI/GLID';
 import Address from '../../UI/Address';
+import Currency from '../../UI/Currency';
 
 const useInfoPanelStyles = makeStyles(theme => ({
   root: {
@@ -32,8 +39,12 @@ const useInfoPanelStyles = makeStyles(theme => ({
   }
 }));
 
-function InfoPanel({ title, children, onEdit, className, ...props }) {
+function InfoPanel({ title, children, editable, onEdit, className, ...props }) {
   const classes = useInfoPanelStyles();
+
+  if (editable === undefined) {
+    editable = true;
+  }
 
   return (
     <Card
@@ -44,38 +55,28 @@ function InfoPanel({ title, children, onEdit, className, ...props }) {
       <CardContent
         className={classes.cardContent}
       >
-        <div
-          className={classes.editIconWrapper}
-        >
-          <IconButton onClick={onEdit}>
-            <EditIcon />
-          </IconButton>
-        </div>
+        {editable && (
+          <div
+            className={classes.editIconWrapper}
+          >
+            <IconButton onClick={onEdit}>
+              <EditIcon />
+            </IconButton>
+          </div>
+        )}
+
         <Typography variant="h5" element="h2" paragraph>{title}</Typography>
+
         {children}
       </CardContent>
     </Card>
   );
 }
 
-const useCustomerInfoPanelStyles = makeStyles(theme => ({
-  customerInfoPanel: {
-    marginBottom: theme.spacing(3)
-  }
-}));
 
-function CustomerInfoPanel({ id }) {
-  const classes = useCustomerInfoPanelStyles();
-  const [customer, setCustomer] = React.useState(null);
-
-  React.useEffect(() => {
-    API.Customers.get(id).then(customer => setCustomer(customer));
-  }, [id]);
-
-  return customer && (
-    <div>
+function CustomerInfoPanel({ customer }) {
+  return (
       <InfoPanel
-        className={classes.customerInfoPanel}
         title="Customer Info"
         onEdit={() => alert('edit the customer!')}
       >
@@ -89,29 +90,214 @@ function CustomerInfoPanel({ id }) {
           {customer.phone}
         </Typography>
       </InfoPanel>
-      
-      <InfoPanel
-        title="Shipping Address"
-        onEdit={() => alert('edit the address!')}
-      >
-        <Address data={customer.address}/>
-      </InfoPanel>
-    </div>
   );
 }
 
+function ShippingInfoPanel({ customer }) {
+  return (
+    <InfoPanel
+      title="Shipping Address"
+      onEdit={() => alert('edit the address!')}
+    >
+      <Address data={customer.address} />
+    </InfoPanel>
+  );
+};
+
+function NotesInfoPanel({ notes }) {
+  return (
+    <InfoPanel
+      title="Order Notes"
+      onEdit={() => alert('edit the notes!')}
+    >
+      {notes !== '' && (
+        <Typography>
+          {notes}
+        </Typography>
+      )}
+      {notes === '' && (
+        <Typography color="textSecondary">
+          No notes yet
+        </Typography>
+      )}
+    </InfoPanel>
+  )
+}
+
+const useBillingInfoPanelStyles = makeStyles(theme => ({
+  tableContainer: {
+    marginBottom: theme.spacing(2)
+  },
+  refundIcon: {
+    fontSize: 12,
+    marginLeft: theme.spacing(2)
+  }
+}));
+
+function BillingInfoPanel({ order, onRefundAmountChange }) {
+  const classes = useBillingInfoPanelStyles();
+  let refundAmount = null;
+  const [refundDisplayType, setRefundDisplayType] = React.useState('text');
+
+  const rows = [
+    {
+      title: 'Item Total',
+      content: <Currency value={order.amounts.itemTotalPrice}/>
+    },
+    {
+      title: 'Coupon',
+      content: '--' // TODO Coupon Component
+    },
+    {
+      title: 'Tax',
+      content: <Currency value={order.amounts.tax} />
+    },
+    {
+      title: 'Order Total',
+      content: <Currency value={order.amounts.total} />
+    }
+  ];
+
+  function handleRefundEditClick(event) {
+    setRefundDisplayType('input');
+  }
+
+  function handleRefundSaveClick(event) {
+    console.log(event);
+    setRefundDisplayType('text');
+    onRefundAmountChange(refundAmount);
+  }
+
+  return (
+    <InfoPanel
+      title="Billing"
+      editable={false}
+    >
+      <TableContainer
+        component={Box}
+        className={classes.tableContainer}
+      >
+        <Table
+          size="small"
+        >
+          {rows.map(row => (
+            <TableRow>
+              <TableCell
+                component="th"
+                scope="row"
+              >
+                {row.title}
+              </TableCell>
+              <TableCell>
+                {row.content}
+              </TableCell>
+            </TableRow>
+          ))}
+        </Table>
+      </TableContainer>
+
+      <Typography paragraph>
+        Refund to date:
+        <Currency
+          value={order.amounts.refunded}
+          displayType={refundDisplayType}
+          onValueChange={values => refundAmount = values.floatValue}
+          onSubmit={() => alert('submit!')}
+        />
+        {refundDisplayType === 'text' && (
+          <EditIcon
+            className={classes.refundIcon}
+            onClick={handleRefundEditClick}
+          />
+        )}
+        {refundDisplayType === 'input' && (
+          <SaveIcon
+            className={classes.refundIcon}
+            onClick={handleRefundSaveClick}
+          />
+        )}
+      </Typography>
+    </InfoPanel>
+  );
+}
+
+function LogInfoPanel({ order }) {
+  return (
+    <InfoPanel
+      title="Activity Log"
+      editable={false}
+    >
+      <Typography color="textSecondary">Not implemented yet</Typography>
+    </ InfoPanel>
+  );
+}
+
+const gridSpacing = 2;
+
+const useStyles = makeStyles(theme => ({
+  root: {},
+  gridItem: {
+    '& > :not(:last-child)': {
+      marginBottom: theme.spacing(gridSpacing)
+    }
+  }
+}));
+
 export default function OrderViewer({ orderId }) {
+  const classes = useStyles();
   const [order, setOrder] = React.useState(null);
+  const [customer, setCustomer] = React.useState(null);
 
   React.useEffect(() => {
     API.Orders.get(parseGLID(orderId)).then(order => setOrder(order));
   }, [orderId]);
+
+  React.useEffect(() => {
+    if (order) {
+      API.Customers.get(order.customerId).then(customer => setCustomer(customer));
+    }
+  }, [order]);
+
+  function handleRefundAmountChange(amount) {
+    API.Orders.update(parseGLID(orderId), {
+      amounts: { ...order.amounts, refunded: amount }
+    }).then(order => setOrder(order));
+  }
   
   return order && (
     <div>
-      <Grid container>
-        <Grid item sm={12} md={5}>
-          <CustomerInfoPanel id={order.customerId}/>
+      <Grid container spacing={gridSpacing}>
+        <Grid
+          item
+          className={classes.gridItem}
+          sm={12}
+          md={4}
+        >
+          {customer && (
+            <React.Fragment>
+              <CustomerInfoPanel customer={customer} />
+              <ShippingInfoPanel customer={customer} />
+            </React.Fragment>
+          )}
+        </Grid>
+
+        <Grid
+          item
+          className={classes.gridItem}
+          sm={12}
+          md={5}
+        >
+          <NotesInfoPanel notes={order.notes} />          
+          <BillingInfoPanel order={order} onRefundAmountChange={handleRefundAmountChange} />
+        </Grid>
+
+        <Grid
+          item
+          className={classes.gridItem}
+          sm={12}
+          md={3}
+        >
+          <LogInfoPanel order={order} />
         </Grid>
       </Grid>
     </div>
---
+++ src/Components/UI/Currency.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import NumberFormat from 'react-number-format';
+
+export default function Currency(props) {
+  const _props = {
+    displayType: 'text',
+    prefix: '$',
+    decimalScale: 2,
+    fixedDecimalScale: true,
+    ...props
+  };
+
+  return <NumberFormat {..._props}/>
+}
