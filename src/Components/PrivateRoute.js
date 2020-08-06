06a71c7b8490fe513d4a2fca6ab9f98041a1977c
James Sayer <james.sayer0908@gmail.com>
2020-08-06 14:13:47 -0700

    Formatting and added role check

--- src/Components/PrivateRoute.js
+++ src/Components/PrivateRoute.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 
-import Auth from '../Services/Auth';
+import Auth, { roles } from '../Services/Auth';
 
-export default function PrivateRoute({ component: Component, ...rest }) {
+export default function PrivateRoute({ component: Component, isAdminRoute, ...rest }) {
   return (
     <Route {...rest} render={props => {
       const currentUser = Auth.currentUserValue;
       if (!currentUser) {
         // not logged in so redirect to login page with the return url
         return <Redirect to={{ pathname: '/', state: { from: props.location } }} />
+      }
+
+      if (isAdminRoute && currentUser.role === roles.manufacturer) {
+        return <Redirect to={{ pathname: '/', state: { flash: 'Access denied' } }} />
       }
 
       // authorised so return component
