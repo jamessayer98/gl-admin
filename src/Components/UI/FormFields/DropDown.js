c932ef0fca7b6f368d9199653ec23a215ce8c935
James Sayer <james.sayer0908@gmail.com>
2020-08-12 17:26:56 -0700

    Added defaults for dataSource, other minor improvements

--- src/Components/UI/FormFields/DropDown.js
+++ src/Components/UI/FormFields/DropDown.js
@@ -3,35 +3,37 @@ import { Select, MenuItem, InputLabel, FormControl, makeStyles, FormHelperText }
 import { getFieldError } from '../../../Helpers/FormHelpers'
 
 const useStyles = makeStyles((theme) => ({
-  formControl: {
-    margin: theme.spacing(1)
-  }
 }));
 
 export default function DropDown({ field, form: { touched, errors }, ...props }) {
-  const { label, dataSource, dataKey, dataValue, ...rest } = props
+  const { label, dataSource, dataKey, dataValue, ...rest } = props;
   const classes = useStyles();
   let items = null;
 
-  const {wasTouched, errorMsg} = getFieldError(field.name, touched, errors)
+  const { wasTouched, errorMsg } = getFieldError(field.name, touched, errors)
 
   if (dataSource) {
-    items = dataSource.map(item => (
-      <MenuItem key={item[dataValue]} value={item[dataValue]}>{item[dataKey]}</MenuItem>
-    ));
+    if (dataKey && dataValue) {
+      items = dataSource.map((item, index) => (
+        <MenuItem key={index} value={item[dataValue]}>{item[dataKey]}</MenuItem>
+      ));
+    } else {
+      items = dataSource.map((item, index) => (
+        <MenuItem key={index} value={item.value}>{item.label}</MenuItem>
+      ));
+    }
   }
+
   return (
     <FormControl
       variant="outlined"
-      className={classes.formControl}
-      fullWidth
       error={wasTouched && errorMsg && true}
+      {...rest}
     >
       <InputLabel id={`${field.name}-label`}>{label}</InputLabel>
       <Select
         labelId={`${field.name}-label`}
         {...field}
-        {...rest}
       >
         {items}
       </Select>
